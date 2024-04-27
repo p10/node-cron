f180e0b5fc07eef2a06390bcda820614a6dccb2c
p10 <pio.was@gmail.com>
2024-04-27 17:28:38 +0200

    __diranme; type module

--- src/background-scheduled-task/index.js
+++ src/background-scheduled-task/index.js
@@ -2,55 +2,56 @@ import EventEmitter from "events";
 import path from "path";
 import { fork } from "child_process";
 import * as uuid from "uuid";
+const __dirname = path.resolve();
 const daemonPath = `${__dirname}/daemon.js`;
 class BackgroundScheduledTask extends EventEmitter {
-    constructor(cronExpression, taskPath, options) {
-        super();
-        if (!options) {
-            options = {
-                scheduled: true,
-                recoverMissedExecutions: false,
-            };
-        }
-        this.cronExpression = cronExpression;
-        this.taskPath = taskPath;
-        this.options = options;
-        this.options.name = this.options.name || uuid.v4();
-        if (options.scheduled) {
-            this.start();
-        }
+  constructor(cronExpression, taskPath, options) {
+    super();
+    if (!options) {
+      options = {
+        scheduled: true,
+        recoverMissedExecutions: false,
+      };
     }
-    start() {
-        this.stop();
-        this.forkProcess = fork(daemonPath);
-        this.forkProcess.on('message', (message) => {
-            switch (message.type) {
-                case 'task-done':
-                    this.emit('task-done', message.result);
-                    break;
-            }
-        });
-        let options = this.options;
-        options.scheduled = true;
-        this.forkProcess.send({
-            type: 'register',
-            path: path.resolve(this.taskPath),
-            cron: this.cronExpression,
-            options: options
-        });
+    this.cronExpression = cronExpression;
+    this.taskPath = taskPath;
+    this.options = options;
+    this.options.name = this.options.name || uuid.v4();
+    if (options.scheduled) {
+      this.start();
     }
-    stop() {
-        if (this.forkProcess) {
-            this.forkProcess.kill();
-        }
+  }
+  start() {
+    this.stop();
+    this.forkProcess = fork(daemonPath);
+    this.forkProcess.on("message", (message) => {
+      switch (message.type) {
+        case "task-done":
+          this.emit("task-done", message.result);
+          break;
+      }
+    });
+    let options = this.options;
+    options.scheduled = true;
+    this.forkProcess.send({
+      type: "register",
+      path: path.resolve(this.taskPath),
+      cron: this.cronExpression,
+      options: options,
+    });
+  }
+  stop() {
+    if (this.forkProcess) {
+      this.forkProcess.kill();
     }
-    pid() {
-        if (this.forkProcess) {
-            return this.forkProcess.pid;
-        }
+  }
+  pid() {
+    if (this.forkProcess) {
+      return this.forkProcess.pid;
     }
-    isRunning() {
-        return !this.forkProcess.killed;
-    }
+  }
+  isRunning() {
+    return !this.forkProcess.killed;
+  }
 }
 export default BackgroundScheduledTask;
